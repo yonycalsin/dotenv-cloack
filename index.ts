3c9b533a78a73917b5b041efbbe2b1b72ea06a85
Yoni Calsin <yonicb20@gmail.com>
2020-07-21 18:14:10 -0500

    feat: 🎸 created getOptions and rex functions

---
+++ index.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+const rex = /^\s*([\w.-]+)\s*=\s*(.*)?\s*$/;
+
+const getOptions = () => {
+   try {
+      const data = fs.readFileSync(path.resolve('envrc.json'), {
+         encoding: 'utf-8',
+      });
+      return JSON.parse(data);
+   } catch {
+      return {
+         '@ignore': [],
+      };
+   }
+};
